da539a21223544a59413d5188cf126745d378315
Angeredspy <christo.graham1@gmail.com>
2023-09-25 02:20:08 -0400

    * States the selector and the templateUrl for post-create-component * Configures the post-create-component class & props * Configures the onAddPost method

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-create', //The selector property defines the custom HTML tag that
+  //Angular will replace with this component.<app-post-create></app-post-create>
+
+  templateUrl: './post-create.component.html', //The templateUrl property tells Angular the location of the HTML file that contains
+  //the template for the component. The template is essentially the HTML layout that the component will render.
+})
+export class PostCreateComponent {
+  numRows = 20;
+  enteredValue = 'Type Here!';
+  newPost = 'No Content!';
+
+  onAddPost = () => {
+    this.newPost = this.enteredValue;
+  };
+}
