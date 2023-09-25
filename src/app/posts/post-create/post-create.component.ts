488b5eeb05ee2d2fcce8b42db8d18ccf6012bb41
Angeredspy <christo.graham1@gmail.com>
2023-09-25 03:19:15 -0400

    Uses custom CSS for the post component

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-post-create', //The selector property defines the custom HTML tag that
   //Angular will replace with this component.<app-post-create></app-post-create>
 
   templateUrl: './post-create.component.html', //The templateUrl property tells Angular the location of the HTML file that contains
   //the template for the component. The template is essentially the HTML layout that the component will render.
+
+  styleUrls: ['./post.create.component.css']
 })
 export class PostCreateComponent {
   numRows = 20;
