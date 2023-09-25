b54b5c700a085b38b0a83b20df0328b3a315dce6
Angeredspy <christo.graham1@gmail.com>
2023-09-25 03:20:08 -0400

    Adds MatButtonModule & MatToolbarModule Imports the new header component & adds declaration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,25 @@ import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule } from '@angular/material/input'
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button'
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { Header } from './header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PostCreateComponent
-  ],
+  declarations: [AppComponent, PostCreateComponent, Header],
   imports: [
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
     MatInputModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
