4c19a542850e2ceed385a824c6a0f62750025152
Angeredspy <christo.graham1@gmail.com>
2023-10-01 19:01:19 -0400

    * * Imports NgForm Refactors onAddPost to accept a form * Adds form validity checking * Gets post values from the form inputs

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { Post } from '../post.model';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-post-create', //The selector property defines the custom HTML tag that
   //Angular will replace with this component.<app-post-create></app-post-create>
@@ -16,10 +17,14 @@ export class PostCreateComponent {
   @Output()
   postCreated = new EventEmitter<Post>();
 
-  onAddPost = () => {
+  onAddPost = (form: NgForm) => {
+      if (form.invalid) {
+        return;
+      }
+
     const post : Post = {
-      title: this.enteredTitle, 
-      content: this.enteredContent
+      title: form.value.title, 
+      content: form.value.content
     }
     this.postCreated.emit(post)
   };
