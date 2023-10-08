6983103d331dd339f2d9476788d6a959ec2e8d22
Angeredspy <christo.graham1@gmail.com>
2023-10-08 00:12:38 -0400

    * Imports Injectable for dependency injection * Imports subject for Observables & Subscriptions Initializes a new Subject. * Returns an Observable within getPostUpdateListene * Refactors addPosts to include the Subject and adds our list of posts to it which will be noticed by the observable

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,16 +1,24 @@
+import { Injectable } from '@angular/core';
 import { Post } from './post.model';
+import { Subject } from 'rxjs';
 
+@Injectable({providedIn: 'root'})
 export class PostsService {
     private posts : Post[] = []
+    private postsUpdated = new Subject<Post[]>();
 
     getPosts = () => {
-        return [...this.posts]
+        return [...this.posts];
     }
 
-    addPosts = (post: Post) => {
-        const addedPosts: Post = {title: post.title, content: post.content}
-        this.posts.push(addedPosts)
+    getPostsUpdateListener = () => {
+        return this.postsUpdated.asObservable();
     }
 
+    addPosts = (title: string, content: string) => {
+        const addedPosts: Post = {title: title, content: content}
+        this.posts.push(addedPosts);
+        this.postsUpdated.next([...this.posts]);
+    }
 }
 
